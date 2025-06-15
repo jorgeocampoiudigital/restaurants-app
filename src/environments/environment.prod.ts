bd389a195a0b7caa339fe141bde40202605ae836
jorgeocampoiudigital <101612518+jorgeocampoiudigital@users.noreply.github.com>
2025-06-15 18:10:02 -0500

    Deploy-App
    Deploy-App

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDWVZgro8E6zW3pzuBdsdVU1HAgZ1uU5Vg",
+    authDomain: "restaurants-38bb2.firebaseapp.com",
+    projectId: "restaurants-38bb2",
+    storageBucket: "restaurants-38bb2.firebasestorage.app",
+    messagingSenderId: "746883622240",
+    appId: "1:746883622240:web:0a5a336be05c24394f6a05",
+    measurementId: "G-7D29QKWRJ7"
+  }
+};
